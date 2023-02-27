f6e9d94960f0601904ee3d67c49af2a92f2cbf28
simmmpleweb <hello@simmmple.com>
2023-02-27 16:29:31 +0200

    Changed base paths

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import AuthLayout from "layouts/auth";
 const App = () => {
   return (
     <Routes>
-      <Route path="auth/*" element={<AuthLayout />} />
-      <Route path="admin/*" element={<AdminLayout />} />
+      <Route path="/auth/*" element={<AuthLayout />} />
+      <Route path="/admin/*" element={<AdminLayout />} />
       <Route path="/" element={<Navigate to="/admin" replace />} />
     </Routes>
   );
