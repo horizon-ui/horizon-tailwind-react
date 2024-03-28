679aa6db275bc2461d52cdf30f892fc0b804c3dc
Acepades <ac3pad3s@gmail.com>
2024-03-29 00:44:24 +0100

    meh

--- src/components/sidebar/RTL.jsx
+++ src/components/sidebar/RTL.jsx
@@ -3,7 +3,6 @@
 import { HiX } from "react-icons/hi";
 import Links from "./components/Links";
 
-import SidebarCard from "components/sidebar/componentsrtl/SidebarCard";
 import routes from "routes.js";
 
 const Sidebar = ({ open, onClose }) => {
@@ -31,11 +30,6 @@ const Sidebar = ({ open, onClose }) => {
       <ul className="mb-auto pt-1">
         <Links routes={routes} />
       </ul>
-
-      {/* Free Horizon Card */}
-      <div className="flex justify-center">
-        <SidebarCard />
-      </div>
 
       {/* Nav item end */}
     </div>
